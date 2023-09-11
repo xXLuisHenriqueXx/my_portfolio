6512422eefbd1ae776059d8eff4762e8a9b8c713
Luis Henrique Cardoso <luishenri567@gmail.com>
2023-09-11 15:50:37 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroudImage: {
+        'home-bg': "url('./images/sec_home.svg')"
+      }
+    },
   },
   plugins: [],
 }
